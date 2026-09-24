21309154d0bc6a79011ebdda11b7b8fd6fcca32e
AnasMubarakYasin <bladerlaiga.97@gmail.com>
2026-09-25 00:24:47 +0800

    chore: startup script status

--- startup.sh
+++ startup.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -e
 
-printenv
-
 echo "=== Running seeder ==="
 cd /app/instance && go run database/seeder/main.go --mode=reset
 
 echo "=== Starting supervisor ==="
 exec supervisord -n -c /etc/supervisor/supervisord.conf
+
+echo "=== Status supervisor ==="
+exec supervisorctl status
